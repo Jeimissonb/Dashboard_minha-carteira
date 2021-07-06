378d94da4da0c1912449c76d4d263f5e63504e08
Jeimissonb <jeimissonb@hotmail.com>
2021-07-06 17:07:11 -0300

    Adicionando emoji dinâmico ao welcome do header, utilizando array e useMemo do react

--- minha-carteira/src/App.tsx
+++ minha-carteira/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from './Components/Layout';
 import './styles/GlobalStyles.scss'
+import Themes from '../src/styles/styled'
+import Dark from './styles/themes/dark'
 
 const App: React.FC = () => {
     return (
-    
+        
         <Layout />
         
     )
--- minha-carteira/src/Components/MainHeader/index.tsx
+++ minha-carteira/src/Components/MainHeader/index.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './style.scss'
+import React, {useMemo} from 'react'
+import Style from './style.module.scss'
+import emojis from '../../utils/emojis'
+
 
 const MainHeader: React.FC = () => {
+
+   const emoji = useMemo(()=>{
+       const indice = Math.floor(Math.random()*emojis.length)
+       return emojis[indice];
+   },[]);
+   
     return (
-        <div className="ContainerMH">
-             <h1 >MainHeader</h1>
+     
+        <div className={Style.Container}>
+             <h1 >Toogle</h1>
+
+            <div className={Style.Profile}>
+                <div className={Style.Welcome}>Olá, {emoji}</div>
+                <div className={Style.Username}>Jeimisson Barbosa</div>
+            </div>
+
         </div>
         
     )
