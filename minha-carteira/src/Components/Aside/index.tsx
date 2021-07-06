55de6b2494b7e09ac475618a390dbcbcd0b52dfd
Jeimissonb <jeimissonb@hotmail.com>
2021-07-06 18:20:39 -0300

    Criando menus do componente Aside (menu lateral) e utilizando a biblioteca react-icons/md

--- minha-carteira/src/Components/Aside/index.tsx
+++ minha-carteira/src/Components/Aside/index.tsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import Style from './style.module.scss'
+import logoImg from '../../assets/logo.svg'
+import {
+    MdDashboard,
+    MdArrowDownward,
+    MdArrowUpward,
+    MdExitToApp
+} from 'react-icons/md'
+
 
 const Aside: React.FC = () => {
     return (
         <div className={Style.Container}>
-        <h1> Aside </h1>
+            <div className={Style.Header}>
+                <img className={Style.logoImg} src={logoImg} alt="logo Minha carteira" />
+                <span className={Style.Title}>Minha Carteira</span>
+            </div>
+            <div className={Style.MenuContainer}>
+                <a className = {Style.MenuItemLink} href="#">
+                    <MdDashboard/>
+                    Dashboard
+                </a>
+                <a className = {Style.MenuItemLink} href="#">
+                    <MdArrowUpward/>
+                    Entradas
+                </a>
+                <a className = {Style.MenuItemLink} href="#">
+                    <MdArrowDownward/>
+                    Saídas
+                </a>
+                <a className = {Style.MenuItemLink} href="#">
+                    <MdExitToApp/>
+                    Sair
+                </a>
+            </div>
         </div>
     )
 }
